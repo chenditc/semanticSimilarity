462485972e3a03ae08fc9eed11346fef104b72c8
Danny <chenditc@umich.edu>
2014-02-15 22:43:35 -0500

    Lin comparator working. Take long time at first run to generate meta data

--- src/com/dichen/semanticSim/SemanticSim.java
+++ src/com/dichen/semanticSim/SemanticSim.java
@@ -1,8 +1,6 @@
 package com.dichen.semanticSim;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -19,76 +17,45 @@ import dkpro.similarity.algorithms.api.TextSimilarityMeasure;
 import dkpro.similarity.algorithms.lexical.ngrams.WordNGramJaccardMeasure;
 import dkpro.similarity.algorithms.lsr.LexSemRelationComparator;
 import dkpro.similarity.algorithms.lsr.LexSemResourceComparator;
+import dkpro.similarity.algorithms.lsr.path.LinComparator;
 
 public class SemanticSim {
 
     /**
      * @param args
      */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        
-     // this similarity measure is defined in the dkpro.similarity.algorithms.lexical-asl package
-     // you need to add that to your .pom to make that example work
-     // there are some examples that should work out of the box in dkpro.similarity.example-gpl 
-     TextSimilarityMeasure measure2 = new WordNGramJaccardMeasure(3);    // Use word trigrams
-     
-     LexicalSemanticResource resource;
-    try {
-        // Get resource from local file
-        resource = ResourceFactory.getInstance().get("wordnet", "en");
-        
-        
-        /*
-         *  Lexical relation can be:
-         *        antonymy,
-         *        synonymy 
-         *          
-         *  Semantic relation can be:
-         *        holonymy,
-         *        hypernymy,
-         *        hyponymy,
-         *        meronymy,
-         *        cohyponymy,
-         *        other 
-         */
-        // Put all relation into collection.
-        // TODO: might specify some of them.
-        Set<LexicalRelation> lexicalRelations = new HashSet<LexicalSemanticResource.LexicalRelation>(Arrays.asList(LexicalRelation.values()));
-        Set<SemanticRelation> semanticRelations = new HashSet<LexicalSemanticResource.SemanticRelation>(Arrays.asList(SemanticRelation.values()));
+    public static void main(String[] args) {     
+        LexicalSemanticResource resource;
+        try {
+            // Get resource from local file
+            resource = ResourceFactory.getInstance().get("wordnet", "en");
+            
 
-        TextSimilarityMeasure measure = new LexSemRelationComparator(resource, lexicalRelations, semanticRelations);
-        
-        String[] tokens1 = "job".split(" ");   
-        String[] tokens2 = "bike".split(" ");
-        List<String> token1 = Arrays.asList(tokens1);
-        List<String> token2 = Arrays.asList(tokens2);
+            TextSimilarityMeasure measure = new LinComparator(resource);
+            
+            String[] tokens1 = "job is book".split(" ");   
+            String[] tokens2 = "bike are text".split(" ");
+            List<String> token1 = Arrays.asList(tokens1);
+            List<String> token2 = Arrays.asList(tokens2);
 
-        //TODO: probably we should lemmatize the input before pass in. Not documented.
-        double score;
-        score = measure.getSimilarity(token1, token2);
-        System.out.println("Similarity: " + score);
+            //TODO: probably we should lemmatize the input before pass in. Not documented.
+            double score;
+            score = measure.getSimilarity(token1, token2);
+            System.out.println("Similarity: " + score);
 
 
-    } catch (ResourceLoaderException e1) {
-        // TODO Auto-generated catch block
-        e1.printStackTrace();
-    } catch (LexicalSemanticResourceException e1) {
-        // TODO Auto-generated catch block
-        e1.printStackTrace();
-    } catch (SimilarityException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
-     
+        } catch (ResourceLoaderException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (LexicalSemanticResourceException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (SimilarityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-
-     
-
-
-        
-        
-        System.out.print("Hello world");
+        System.out.print("Finish running");
     }
 
 }
