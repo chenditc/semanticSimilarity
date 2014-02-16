c7675b6658e2a8b659163c7391301c84febb2bee
Danny <chenditc@umich.edu>
2014-02-15 22:03:36 -0500

    Working example using wordnet topology

--- README.md
+++ README.md
@@ -7,4 +7,11 @@ Setup:
 
 1. Install Maven 
 2. Copy settings.xml to ~/.m2/
-3. import project as maven project
+3. Import project as maven project
+4. Export the project path to environment variable DKPRO_HOME.
+
+
+Reference:
+
+https://code.google.com/p/dkpro-similarity-asl/wiki/SettingUpTheResources
+https://code.google.com/p/dkpro-core-asl/wiki/Configuration
--- src/com/dichen/semanticSim/SemanticSim.java
+++ src/com/dichen/semanticSim/SemanticSim.java
@@ -2,12 +2,23 @@ package com.dichen.semanticSim;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import de.tudarmstadt.ukp.dkpro.lexsemresource.Entity;
+import de.tudarmstadt.ukp.dkpro.lexsemresource.LexicalSemanticResource;
+import de.tudarmstadt.ukp.dkpro.lexsemresource.LexicalSemanticResource.LexicalRelation;
+import de.tudarmstadt.ukp.dkpro.lexsemresource.LexicalSemanticResource.SemanticRelation;
+import de.tudarmstadt.ukp.dkpro.lexsemresource.core.ResourceFactory;
+import de.tudarmstadt.ukp.dkpro.lexsemresource.exception.LexicalSemanticResourceException;
+import de.tudarmstadt.ukp.dkpro.lexsemresource.exception.ResourceLoaderException;
 import dkpro.similarity.algorithms.*;
 import dkpro.similarity.algorithms.api.SimilarityException;
 import dkpro.similarity.algorithms.api.TextSimilarityMeasure;
 import dkpro.similarity.algorithms.lexical.ngrams.WordNGramJaccardMeasure;
+import dkpro.similarity.algorithms.lsr.LexSemRelationComparator;
+import dkpro.similarity.algorithms.lsr.LexSemResourceComparator;
 
 public class SemanticSim {
 
@@ -20,25 +31,60 @@ public class SemanticSim {
      // this similarity measure is defined in the dkpro.similarity.algorithms.lexical-asl package
      // you need to add that to your .pom to make that example work
      // there are some examples that should work out of the box in dkpro.similarity.example-gpl 
-     TextSimilarityMeasure measure = new WordNGramJaccardMeasure(3);    // Use word trigrams
-
-     String[] tokens1 = "This is a short example text.".split(" ");   
-     String[] tokens2 = "This is an example text.".split(" ");
+     TextSimilarityMeasure measure2 = new WordNGramJaccardMeasure(3);    // Use word trigrams
      
-     List<String> token1 = Arrays.asList(tokens1);
-     List<String> token2 = Arrays.asList(tokens2);
-
-     
-     // only works from 2.1.0-SHAPSHOT onwards, for previous versions you need to convert to Collection<String> first
-     double score;
+     LexicalSemanticResource resource;
     try {
+        // Get resource from local file
+        resource = ResourceFactory.getInstance().get("wordnet", "en");
+        
+        
+        /*
+         *  Lexical relation can be:
+         *        antonymy,
+         *        synonymy 
+         *          
+         *  Semantic relation can be:
+         *        holonymy,
+         *        hypernymy,
+         *        hyponymy,
+         *        meronymy,
+         *        cohyponymy,
+         *        other 
+         */
+        // Put all relation into collection.
+        // TODO: might specify some of them.
+        Set<LexicalRelation> lexicalRelations = new HashSet<LexicalSemanticResource.LexicalRelation>(Arrays.asList(LexicalRelation.values()));
+        Set<SemanticRelation> semanticRelations = new HashSet<LexicalSemanticResource.SemanticRelation>(Arrays.asList(SemanticRelation.values()));
+
+        TextSimilarityMeasure measure = new LexSemRelationComparator(resource, lexicalRelations, semanticRelations);
+        
+        String[] tokens1 = "job".split(" ");   
+        String[] tokens2 = "bike".split(" ");
+        List<String> token1 = Arrays.asList(tokens1);
+        List<String> token2 = Arrays.asList(tokens2);
+
+        //TODO: probably we should lemmatize the input before pass in. Not documented.
+        double score;
         score = measure.getSimilarity(token1, token2);
         System.out.println("Similarity: " + score);
 
+
+    } catch (ResourceLoaderException e1) {
+        // TODO Auto-generated catch block
+        e1.printStackTrace();
+    } catch (LexicalSemanticResourceException e1) {
+        // TODO Auto-generated catch block
+        e1.printStackTrace();
     } catch (SimilarityException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
     }
+     
+
+
+     
+
 
         
         
